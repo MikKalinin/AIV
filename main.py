8a0ab70fc425d49b54c5a676f8cfea7869787dde
Smotne <mkalinin418@gmail.com>
2024-08-02 13:23:51 +0300

    commit

--- main.py
+++ main.py
@@ -1,20 +1,36 @@
 from threading import Thread
 import keyboard1 as kb
 from queue import Queue
+import time
 
 q = Queue()
 
 
-def tracker():
+def constrain(val, min_val, max_val):
+    if val < min_val: return min_val
+    if val > max_val: return max_val
+    return val
+
+
+def pid(input, setpoint, kp, ki, kd, dt, minOut, maxOut):
+    err = setpoint - input
+    integral = 0
+    prevErr = 0
+    integral = constrain(integral + err * dt * ki, minOut, maxOut)
+    d = (err - prevErr) / dt
+    prevErr = err
+    return constrain(err * kp + integral + d * kd, minOut, maxOut)
+
+
+def tracker1():
     while True:
         track = q.get()
-        print(track)
-        if 0 < track < 100:
+
+        if track > 0:
             kb.key_press(kb.SC_RIGHT)
-        if -100 < track < 0:
+        if track < 0:
             kb.key_press(kb.SC_LEFT)
 
 
-
-th = Thread(target=tracker, daemon=True)
+th = Thread(target=tracker1, daemon=True)
 
--- window.py
+++ window.py
@@ -3,12 +3,25 @@ import time
 import numpy as np
 import cv2
 import imutils
-from main import q, th
+from main import q, th, pid
+import keyboard
 
 print('waiting for 2 seconds...')
 time.sleep(2)
 
 title = './logo1.png'
+
+contlist = list()
+contorsize = list()
+timer1 = 0
+t1 = 0
+tf = 0
+megamoveFlag = 0
+megamoveFlag2 = 0
+firstFlag1 = 0
+pidt = 0
+pidt2 = 0
+kayw = 'start'
 
 nfs_window_location = None
 searching_attempt = 1
@@ -69,34 +82,108 @@ while True:
     contours = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     contours = contours[0]
 
+    X = result.shape[0]
+    Y = result.shape[1]
+    startP = (Y//2, X)
+
     if contours:
         contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
         cv2.drawContours(result, contours, -1, (255, 0, 0), 1)
 
-        idx = 0
-        (x, y, w, h) = cv2.boundingRect(contours[idx])
+        for id in range(len(contours)):
+            (x, y, w, h) = cv2.boundingRect(contours[id])
 
-        cv2.rectangle(result, (x, y), (x+w, y+h), (0, 255, 0), 1)
+            cv2.rectangle(result, (x, y), (x+w, y+h), (0, 255, 0), 1)
 
-        (x1, y1), radius = cv2.minEnclosingCircle(contours[idx])
-        center = (int(x1), int(y1))
-        radius = int(radius)
+            contlist.append((x + w // 2, y + h // 2))
 
-        X = result.shape[0]
-        Y = result.shape[1]
-        startP = (Y//2, X)
+            (x1, y1), radius = cv2.minEnclosingCircle(contours[id])
+            center = (int(x1), int(y1))
+            radius = int(radius)
 
-        cv2.circle(result, center, radius, (0, 255, 0), 1)
-        cv2.line(result, startP, center, (0, 255, 0), 1)
 
-        track = x1 - Y//2
+            cv2.circle(result, center, radius, (0, 255, 0), 1)
+            cv2.line(result, startP, center, (0, 255, 0), 1)
 
-        if q.empty():
-            q.put(track)
+            track = x1 - Y//2
+
+            if q.empty():
+                q.put(track)
+
+    max_large = 0
+    pass_x = 0
+    poss_y = 0
+    poss_x1 = 0
+    poss_y1 = 0
+    max_id = len(contours)-1
+
+    for id in range(len(contours)):
+        poss_x1, poss_y1 = contlist[id]
+        if max_id == id:
+            poss_x, poss_y = contlist[id]
+
+    if t1 + 0.1 < time.perf_counter():
+        t1 = time.perf_counter()
+        tf = 1
+    else:
+        tf = 0
+    tf = 1
+
+    xm, ym = pyautogui.position()
+    xw, yw, _, _ = nfs_window_location
+
+    pidt = time.perf_counter() - pidt2
+    if xm > xw and xm < xw + window_resolution[0] and ym > yw and ym < yw + window_resolution[1] and megamoveFlag == 0:
+        if poss_x != 0:
+            pidg = pid(poss_x, startP[0], 10.0, -0.2, 0.2, pidt, -1, 1)
+            if pidg == -1:
+                time.sleep(0.01)
+                kayw = 'd'
+                image = cv2.putText(numpix, 'd' + str(poss_x), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+                time.sleep(0.2)
+                pidt2 = time.perf_counter()
+            elif pidg == 1:
+                time.sleep(0.01)
+                kayw = 'a'
+                image = cv2.putText(numpix, 'a' + str(poss_x), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+                time.sleep(0.2)
+                pidt2 = time.perf_counter()
+
+        if len(contlist) == 0 and megamoveFlag == 0:
+            image = cv2.putText(numpix, 'f', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+            if kayw == 'd':
+                image = cv2.putText(numpix, 'fd', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+                time.sleep(0.05)
+            elif kayw == 'a':
+                image = cv2.putText(numpix, 'fa', (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+                time.sleep(0.05)
+    if megamoveFlag == 1:
+        image = cv2.putText(numpix, 'w', (50, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 0, 0), 2, cv2.LINE_AA)
+
+    contlist = []
+    contorsize = []
 
     cv2.imshow('result', result)
     cv2.imshow('mask', mask)
+
+    if megamoveFlag == 0:
+        time.sleep(0.01)
+
+    if keyboard.is_pressed('t'):
+        if megamoveFlag == 0 and megamoveFlag2 == 1:
+            megamoveFlag = 1
+            megamoveFlag2 = 0
+        elif megamoveFlag == 1 and megamoveFlag2 == 1:
+            megamoveFlag = 0
+            megamoveFlag2 = 0
+            time.sleep(5)
+    else:
+        megamoveFlag2 = 1
+    if firstFlag1 == 0:
+        time.sleep(3)
+        firstFlag1 = 1
+
     if cv2.waitKey(1) == 27:
         break
 
